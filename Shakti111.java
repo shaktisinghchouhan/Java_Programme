e67d750322c07d69a728779215131fe42cf7fc29
Shakti Singh <shaktisinghchouhan30@gmail.com>
2023-08-15 17:03:41 +0530

    after staged goto commit

--- Shakti111.java
+++ Shakti111.java
@@ -4,5 +4,8 @@ public class Shakti111
 public static void main(String[] args) {
 	System.out.println("Hello Git");
 	System.out.println("Shakti");
+	System.out.println("Before Stage");
+	System.out.println("Before stage01");
+	System.out.println("pp");
 }
 }
